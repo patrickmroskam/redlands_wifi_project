bbf41254a87d21f77c02fdfa14caa17b309b2528
Patrick M. Roskam <patrick@calicoders.com>
2025-03-15 22:20:18 -0700

    api key add

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
+    apiKey: "AIzaSyCcslBmrxk2TJWKueWmy_tCXICkl5z1I2c",
     projectId: "redlands-wifi",
     authDomain: "redlands-wifi.firebaseapp.com",
     databaseURL: "https://redlands-wifi.firebaseio.com",
-    storageBucket: "redlands-wifi.appspot.com"
+    storageBucket: "redlands-wifi.appspot.com",
+    messagingSenderId: "654772244549",
+    appId: "1:654772244549:web:6a54aa3045a4aa8a8a3e7b"
 };
 
 // Initialize Firebase
