938aa86055b47de16bba380bb0c1c87ebde8c30d
Patrick M. Roskam <patrick@calicoders.com>
2025-03-16 11:14:33 -0700

    Clean repository for fresh start

--- firebase-config.prod.js
+++
@@ -1,7 +0,0 @@
-// Template for production Firebase configuration
-// Replace these values with your actual production values
-window.FIREBASE_CONFIG = {
-    apiKey: "AIzaSyCcslBmrxk2TJWKueWmy_tCXICkl5z1I2c",
-    messagingSenderId: "654772244549",
-    appId: "1:654772244549:web:6a54aa3045a4aa8a8a3e7b"
-}; 
