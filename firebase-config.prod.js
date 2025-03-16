b98cdf3fdfcfcb84bf313ac53556a6aa1a4fcc3e
Patrick M. Roskam <patrick@calicoders.com>
2025-03-16 09:11:56 -0700

    app id add

--- firebase-config.prod.js
+++ firebase-config.prod.js
@@ -1,7 +1,7 @@
 // Template for production Firebase configuration
 // Replace these values with your actual production values
 window.FIREBASE_CONFIG = {
-    apiKey: "YOUR_API_KEY",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCcslBmrxk2TJWKueWmy_tCXICkl5z1I2c",
+    messagingSenderId: "654772244549",
+    appId: "1:654772244549:web:6a54aa3045a4aa8a8a3e7b"
 }; 
